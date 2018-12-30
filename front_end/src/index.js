eb02185ed15b8c8ca7dd1d33b0de43640173521f
cfernando415 <ca.fernando415@gmail.com>
2018-12-29 20:14:39 -0500

    need to add form to create/edit records

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,21 +2,51 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/api/v1/diseases")
         .then(res => res.json())
         .then(data => {
-            let x_axis = data.map(el => { return el.year });
-            let y_axis = data.map(el => { return el.deaths });
+            const x_axis = data.map(el => { return el.year });
+            const y_axis = data.map(el => { return el.deaths });
             dataHandler(x_axis, y_axis, data);
+            loadNavigation(data);
         })
+    document.getElementById("nav").addEventListener("click", navHandler);
 })
 
-function dataHandler(x_vals, y_vals, data_arr) {
+// let a, b, c, d;
+// let data_x, data_y;
 
-    const learningRate = 0.0042;
-    const optimizer = tf.train.sgd(learningRate);
+// var cubic = function(params,x) {
+//     return params[0] * x*x*x + params[1] * x*x + params[2] * x + params[3];
+// };
+
+// var objective = function(params) {
+//     var total = 0.0;
+//     for(var i=0; i < data_x.length; ++i) {
+//       var resultThisDatum = cubic(params, data_x[i]);
+//       var delta = resultThisDatum - data_y[i];
+//       total += (delta*delta);
+//     }
+//     return total;
+// };
+
+function dataHandler(x_vals, y_vals, data_arr) {
+    
+    const tmp_arr = data_arr.map(el => [el.year, el.deaths])
+    // const tmp = data_arr.filter(el => el.leading_cause === "Diseases of Heart (I00-I09, I11, I13, I20-I51)" && (el.sex ==='M' || el.sex === "Male"));
+
+    // var initial = [1,1,1,1];
+    // data_x = x_vals;
+    // data_y = y_vals;
+
+    // var minimiser = numeric.uncmin(objective,initial);
+
+    // const learningRate = 0.0042;
+    const learningRate = 0.00000001;
+    const optimizer = tf.train.adam(learningRate);
+
     const n = data_arr.length;
-    const sumOfX = x_vals.reduce((a, b) => a + b, 0);
-    const sumOfY = y_vals.reduce((a, b) => a + b, 0);
-    const sumOfX2 = x_vals.map(el => el**2).reduce((a, b) => a + b, 0);
-    const sumOfXY = data_arr.map(el => el.year * el.deaths).reduce((a, b) => a + b, 0);
+    const sumOfX = x_vals.reduce((x, j) => x + j, 0);
+    const sumOfY = y_vals.reduce((x, j) => x + j, 0);
+    const sumOfX2 = x_vals.map(el => el**2).reduce((x, j) => x + j, 0);
+    const sumOfXY = data_arr.map(el => el.year * el.deaths).reduce((x, j) => x + j, 0);
 
     const slope = findSlope(n, sumOfX, sumOfY, sumOfX2, sumOfXY);
     
@@ -26,18 +56,27 @@ function dataHandler(x_vals, y_vals, data_arr) {
     
     let b = tf.variable(tf.scalar(yIntercept));
 
+    // a = tf.variable(tf.scalar(minimiser.solution[0]));
+    // b = tf.variable(tf.scalar(minimiser.solution[1]));
+    // c = tf.variable(tf.scalar(minimiser.solution[2]));
+    // d = tf.variable(tf.scalar(minimiser.solution[3]));
+
+    // a = tf.variable(tf.scalar(1));
+    // b = tf.variable(tf.scalar(2));
+    // c = tf.variable(tf.scalar(1));
+    // d = tf.variable(tf.scalar(0));
+
     tf.tidy(() => {
         if(x_vals.length > 0) {
             const ys = tf.tensor1d(y_vals);
             optimizer.minimize(() => loss(predict(x_vals, m, b), ys));
+            // optimizer.minimize(() => loss(predict(x_vals), ys));
         }
     });
-    
-    // debugger;
 
     const trace1 = {
-        x: data_arr.filter(el => el.sex === 'M').map(el => el.year),
-        y: data_arr.filter(el => el.sex === 'M').map(el => el.deaths),
+        x: data_arr.filter(el => el.sex === 'M' || el.sex === 'Male').map(el => el.year),
+        y: data_arr.filter(el => el.sex === 'M' || el.sex === 'Male').map(el => el.deaths),
         mode: 'markers+Text',
         type: 'scatter',
         name: 'Male',
@@ -46,24 +85,25 @@ function dataHandler(x_vals, y_vals, data_arr) {
         }
     };
 
-    const lineX = [Math.min.apply(null, x_vals), Math.max.apply(null, x_vals)];
-
+    // const lineX = [Math.min.apply(null, x_vals), (Math.max.apply(null, x_vals))];
+    const lineX = x_vals.filter((v, i, a) => a.indexOf(v) === i)
     const ys = tf.tidy(() => predict(lineX, m, b));
+    // const ys = tf.tidy(() => predict(lineX));
     lineY = ys.dataSync();
-
     ys.dispose();
-   
+
     const trace2 = {
         x: lineX,
         y: lineY,
         name: 'trendline',
         mode: 'lines',
-        type: 'scatter'
+        type: 'scatter',
+        // connectgaps: true
     };
 
     const trace3 = {
-        x: data_arr.filter(el => el.sex === 'F').map(el => el.year),
-        y: data_arr.filter(el => el.sex === 'F').map(el => el.deaths),
+        x: data_arr.filter(el => el.sex === 'F' || el.sex === 'Female').map(el => el.year),
+        y: data_arr.filter(el => el.sex === 'F' || el.sex === 'Female').map(el => el.deaths),
         mode: 'markers+Text',
         type: 'scatter',
         name: 'Female',
@@ -71,8 +111,11 @@ function dataHandler(x_vals, y_vals, data_arr) {
             color: 'rgb(233, 121, 127)'
         }
     };
+    
+    const chartTitle = (data_arr.map(e => e.leading_cause).filter((v, i, a) => a.indexOf(v) === i).length > 1) ? "All Diseases" : data_arr.map(e => e.leading_cause).filter((v, i, a) => a.indexOf(v) === i)[0]; 
 
     let layout = {
+        title: chartTitle,
         autosize: true,
         width: 500,
         height: 500,
@@ -87,7 +130,7 @@ function dataHandler(x_vals, y_vals, data_arr) {
     
     let data = [trace1, trace2, trace3];
 
-    Plotly.newPlot('tester', data, layout);
+    Plotly.newPlot('tester', data, layout, {responsive: true});
 
 
 }
@@ -97,6 +140,10 @@ function predict(x_args, m, b) {
     const xs = tf.tensor1d(x_args);
     
     const ys = xs.mul(m).add(b); // y = mx + b
+    // const ys = xs.pow(tf.scalar(3)).mul(a)
+    //     .add(xs.square().mul(b))
+    //     .add(xs.mul(c))
+    //     .add(d);
 
     return ys;
 }
@@ -111,4 +158,32 @@ function findSlope(n, sumOfX, sumOfY, sumOfX2, sumOfXY) {
 
 function findYIntercept(n, sumOfX, sumOfY, slope) {
     return (sumOfY - (slope * sumOfX))/n;
+}
+
+function loadNavigation(arg) {
+    const divRoot = document.getElementById("nav");
+    const categories = arg.map(el => el.leading_cause).filter((x, i, a) => a.indexOf(x) == i);
+
+    for(el of categories) {
+        const divNode = document.createElement("div");
+        divNode.className = "category div_hover";
+        divNode.innerText = el;
+        divRoot.appendChild(divNode);
+    }
+    
+}
+
+function navHandler(e) {
+    const categoryName = e.target.innerText;
+
+    if(e.target.classList.contains("category")){
+        fetch("http://localhost:3000/api/v1/diseases")
+            .then(res => res.json())
+            .then((json) => {              
+                const filteredJSON = json.filter(el => el.leading_cause === categoryName);
+                const x_axis = filteredJSON.map(el => { return el.year });
+                const y_axis = filteredJSON.map(el => { return el.deaths });
+                dataHandler(x_axis, y_axis, filteredJSON);
+            })
+    }
 }
--- leading-causes-death-ML/app/controllers/api/v1/diseases_controller.rb
+++ leading-causes-death-ML/app/controllers/api/v1/diseases_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DiseasesController < ApplicationController
     def index
-        @diseases = Disease.all
+        @diseases = Disease.select("year, leading_cause, sum(deaths) AS deaths, sex").group("year, leading_cause, sex")
         render json: @diseases
     end
 end
